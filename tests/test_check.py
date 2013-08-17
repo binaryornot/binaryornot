6cedfb17afbb3a869336d23cefdfcae1a65754f9
Audrey Roy <audreyr@gmail.com>
2013-08-17 02:18:11 +0200

    Add lots of miserably failing tests.

--- tests/test_check.py
+++ tests/test_check.py
@@ -10,19 +10,35 @@ Tests for `binaryornot` module.
 
 import unittest
 
-from binaryornot import check
+from binaryornot.check import is_binary
 
 
 class TestIsBinary(unittest.TestCase):
 
-    def setUp(self):
-        pass
+    def test_css(self):
+        self.assertFalse(is_binary('tests/files/bootstrap-glyphicons.css'))
 
-    def test_is_binary(self):
-        pass
+    def test_json(self):
+        self.assertFalse(is_binary('tests/files/cookiecutter.json'))
 
-    def tearDown(self):
-        pass
+    def test_eot(self):
+        self.assertTrue(is_binary('tests/files/glyphiconshalflings-regular.eot'))
+
+    def test_otf(self):
+        self.assertTrue(is_binary('tests/files/glyphiconshalflings-regular.otf'))
+
+    def test_svg(self):
+        self.assertFalse(is_binary('tests/files/glyphiconshalflings-regular.svg'))
+
+    def test_ttf(self):
+        self.assertTrue(is_binary('tests/files/glyphiconshalflings-regular.ttf'))
+
+    def test_woff(self):
+        self.assertTrue(is_binary('tests/files/glyphiconshalflings-regular.woff'))
+
+    def test_txt(self):
+        self.assertFalse(is_binary('tests/files/robots.txt'))
+
 
 if __name__ == '__main__':
     unittest.main()
