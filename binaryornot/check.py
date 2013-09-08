e10716b1359efa97893f2f02df8487f9efaf1956
Audrey Roy <audreyr@gmail.com>
2013-09-08 17:48:18 +0200

    Autopep8 everything.

--- binaryornot/check.py
+++ binaryornot/check.py
@@ -35,9 +35,10 @@ def is_binary_string(bytes_to_check):
     :param bytes: A chunk of bytes to check.
     :returns: True if appears to be a binary, otherwise False.
     """
-    
+
     # TODO: rewrite this sanely
-    textchars = ''.join(map(chr, [7,8,9,10,12,13,27] + range(0x20, 0x100)))
+    textchars = ''.join(
+        map(chr, [7, 8, 9, 10, 12, 13, 27] + range(0x20, 0x100)))
     result = bytes_to_check.translate(None, textchars)
     return bool(result)
 
@@ -47,7 +48,7 @@ def is_binary_alt(filename):
     :param filename: File to check.
     :returns: True if it's a binary file, otherwise False.
     """
-    
+
     chunk = get_starting_chunk(filename)
     return is_binary_string(chunk)
 
@@ -57,7 +58,7 @@ def is_binary(filename):
     :param filename: File to check.
     :returns: True if it's a binary file, otherwise False.
     """
-    
+
     # HACK: Works for now, but it would be nice to improve this
     try:
         chunk = get_starting_chunk(filename)
--- tests/test_check.py
+++ tests/test_check.py
@@ -28,19 +28,24 @@ class TestIsBinary(unittest.TestCase):
         self.assertTrue(is_binary('tests/files/.DS_Store'))
 
     def test_eot(self):
-        self.assertTrue(is_binary('tests/files/glyphiconshalflings-regular.eot'))
+        self.assertTrue(
+            is_binary('tests/files/glyphiconshalflings-regular.eot'))
 
     def test_otf(self):
-        self.assertTrue(is_binary('tests/files/glyphiconshalflings-regular.otf'))
+        self.assertTrue(
+            is_binary('tests/files/glyphiconshalflings-regular.otf'))
 
     def test_svg(self):
-        self.assertFalse(is_binary('tests/files/glyphiconshalflings-regular.svg'))
+        self.assertFalse(
+            is_binary('tests/files/glyphiconshalflings-regular.svg'))
 
     def test_ttf(self):
-        self.assertTrue(is_binary('tests/files/glyphiconshalflings-regular.ttf'))
+        self.assertTrue(
+            is_binary('tests/files/glyphiconshalflings-regular.ttf'))
 
     def test_woff(self):
-        self.assertTrue(is_binary('tests/files/glyphiconshalflings-regular.woff'))
+        self.assertTrue(
+            is_binary('tests/files/glyphiconshalflings-regular.woff'))
 
     def test_txt(self):
         self.assertFalse(is_binary('tests/files/robots.txt'))
