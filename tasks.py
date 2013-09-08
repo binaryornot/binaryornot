beb3341d085962564eca850d74f21344c2a5d57f
Audrey Roy <audreyr@gmail.com>
2013-09-08 17:35:52 +0200

    Better Invoke tasks for docs.

--- tasks.py
+++ tasks.py
@@ -1,9 +1,10 @@
 from invoke import task, run
 
 @task
-def clean():
+def clean_docs():
     run("rm -rf docs/_build")
 
-@task('clean')
-def build():
+@task('clean_docs')
+def docs():
     run("sphinx-build docs docs/_build")
+    run("open docs/_build/index.html")
