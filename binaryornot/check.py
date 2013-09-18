3e74ac434e0202f47534a71a2f69ef179be04aa7
Audrey Roy <audreyr@gmail.com>
2013-09-18 02:42:05 +0200

    Get tests passing on 2.7.

--- binaryornot/check.py
+++ binaryornot/check.py
@@ -58,15 +58,22 @@ def is_binary_string(bytes_to_check):
     """
 
     text_ascii_codes = range(32, 256)
+    # text_char_list = [six.int2byte(i) for i in text_ascii_codes]
     textchars = b''.join([six.int2byte(i) for i in text_ascii_codes]) + b'\n\r\t\f\b'
 
     if six.PY2:
+        # Create a translation table
+        delete_chars = dict.fromkeys(bytearray(textchars))
         # Remove the non-text chars from the bytes
-        nontext = bytes_to_check.translate(None, textchars)
+        nontext = bytes_to_check.translate(delete_chars)
         logging.debug("nontext:")
         logging.debug(nontext)
         # Binary if non-text chars are > 30% of the string
-        nontext_ratio = len(nontext) // len(bytes_to_check)
+        logging.debug("len(nontext):")
+        logging.debug(len(nontext))
+        logging.debug("len(bytes_to_check):")
+        logging.debug(len(bytes_to_check))
+        nontext_ratio = float(len(nontext)) / float(len(bytes_to_check))
         logging.debug(nontext_ratio)
         return nontext_ratio > 0.3
 
