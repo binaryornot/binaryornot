db336191ce8d2f55c20010eeebeed6996f71cad5
Audrey Roy <audreyr@gmail.com>
2013-09-22 15:20:06 +0200

    Move everything to helpers except is_binary().

--- binaryornot/check.py
+++ binaryornot/check.py
@@ -11,48 +11,7 @@ Main code for checking if a file is binary or text.
 from __future__ import unicode_literals
 import six
 
-from .helpers import unicode_open
-
-
-class FileNotReadableAsText(Exception):
-
-    """
-    The opened file could not be read as a text file.
-    """
-
-
-def get_starting_chunk(filename):
-    """
-    :param filename: File to open and get the first little chunk of.
-    :returns: Starting chunk of bytes.
-    """
-    try:
-        with unicode_open(filename, 'r') as f:
-            chunk = f.read(1024)
-            return chunk
-    except UnicodeDecodeError:
-        raise FileNotReadableAsText
-
-
-def is_binary_string(bytes_to_check):
-    """
-    :param bytes: A chunk of bytes to check.
-    :returns: True if appears to be a binary, otherwise False.
-    """
-
-    text_ascii_codes = range(32, 256)
-    textchars = b''.join([six.int2byte(i)
-                         for i in text_ascii_codes]) + b'\n\r\t\f\b'
-
-    # Create a translation table
-    delete_chars = dict.fromkeys(bytearray(textchars))
-
-    # Remove the non-text chars from the bytes
-    nontext = bytes_to_check.translate(delete_chars)
-
-    # Binary if non-text chars are > 30% of the string
-    nontext_ratio = float(len(nontext)) / float(len(bytes_to_check))
-    return nontext_ratio > 0.3
+from .helpers import get_starting_chunk, is_binary_string, FileNotReadableAsText
 
 
 def is_binary(filename):
--- binaryornot/helpers.py
+++ binaryornot/helpers.py
@@ -16,6 +16,13 @@ if six.PY2:
     import codecs
 
 
+class FileNotReadableAsText(Exception):
+
+    """
+    The opened file could not be read as a text file.
+    """
+
+
 def unicode_open(filename, *args, **kwargs):
     """
     Opens a file with UTF-8 encoding, in a Python 2- and 3-compatible way.
@@ -37,3 +44,37 @@ def print_as_hex(s):
     """
 
     print(":".join("{0:x}".format(ord(c)) for c in s))
+
+
+def get_starting_chunk(filename):
+    """
+    :param filename: File to open and get the first little chunk of.
+    :returns: Starting chunk of bytes.
+    """
+    try:
+        with unicode_open(filename, 'r') as f:
+            chunk = f.read(1024)
+            return chunk
+    except UnicodeDecodeError:
+        raise FileNotReadableAsText
+
+
+def is_binary_string(bytes_to_check):
+    """
+    :param bytes: A chunk of bytes to check.
+    :returns: True if appears to be a binary, otherwise False.
+    """
+
+    text_ascii_codes = range(32, 256)
+    textchars = b''.join([six.int2byte(i)
+                         for i in text_ascii_codes]) + b'\n\r\t\f\b'
+
+    # Create a translation table
+    delete_chars = dict.fromkeys(bytearray(textchars))
+
+    # Remove the non-text chars from the bytes
+    nontext = bytes_to_check.translate(delete_chars)
+
+    # Binary if non-text chars are > 30% of the string
+    nontext_ratio = float(len(nontext)) / float(len(bytes_to_check))
+    return nontext_ratio > 0.3
