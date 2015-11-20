b748fc18824d3de586ff75ec4cea3fd5a7fc212c
Audrey Roy Greenfeld <aroy@alum.mit.edu>
2015-11-20 08:39:52 -0800

    Start excluding some binary file types based on extension, without opening starting chunk.

--- binaryornot/check.py
+++ binaryornot/check.py
@@ -21,5 +21,13 @@ def is_binary(filename):
     :returns: True if it's a binary file, otherwise False.
     """
     logger.debug('is_binary: %(filename)r', locals())
+
+    # Check if the file extension is in a list of known binary types
+    binary_extensions = ['pyc']
+    for ext in binary_extensions:
+        if filename.endswith(ext):
+            return True
+
+    # Check if the starting chunk is a binary string
     chunk = get_starting_chunk(filename)
     return is_binary_string(chunk)
