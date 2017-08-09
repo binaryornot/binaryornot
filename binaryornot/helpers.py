68c71fecff309e9ae35b9e058f3a3871a949d375
Theo <theo.despoudis@teckro.com>
2017-08-09 07:57:25 +0100

    Fixed-#25 Splits is_binary into multiple functions

--- binaryornot/helpers.py
+++ binaryornot/helpers.py
@@ -48,25 +48,13 @@ else:
     _printable_high_ascii = bytes(range(127, 256))
 
 
-def is_binary_string(bytes_to_check):
+def _check_is_likely_binary(bytes_to_check):
     """
-    Uses a simplified version of the Perl detection algorithm,
-    based roughly on Eli Bendersky's translation to Python:
-    http://eli.thegreenplace.net/2011/10/19/perls-guess-if-file-is-text-or-binary-implemented-in-python/
-
-    This is biased slightly more in favour of deeming files as text
-    files than the Perl algorithm, since all ASCII compatible character
-    sets are accepted as text, not just utf-8.
-
-    :param bytes: A chunk of bytes to check.
-    :returns: True if appears to be a binary, otherwise False.
+    :param bytes_to_check: A chunk of bytes to check
+    :return: True if is likely binary otherwise False.
     """
 
-    # Empty files are considered text files
-    if not bytes_to_check:
-        return False
-
-    # Now check for a high percentage of ASCII control characters
+    # Check for a high percentage of ASCII control characters
     # Binary if control chars are > 30% of the string
     low_chars = bytes_to_check.translate(None, _printable_ascii)
     nontext_ratio1 = float(len(low_chars)) / float(len(bytes_to_check))
@@ -90,7 +78,16 @@ def is_binary_string(bytes_to_check):
     )
     logger.debug('is_likely_binary: %(is_likely_binary)r', locals())
 
-    # then check for binary for possible encoding detection with chardet
+    return is_likely_binary
+
+
+def _check_is_decodable_as_unicode(bytes_to_check):
+    """
+    :param bytes_to_check: A chunk of bytes to check
+    :return: True if is unicode decodable otherwise False.
+    """
+
+    # Check for binary for possible encoding detection with chardet
     detected_encoding = chardet.detect(bytes_to_check)
     logger.debug('detected_encoding: %(detected_encoding)r', locals())
 
@@ -116,6 +113,44 @@ def is_binary_string(bytes_to_check):
 
     logger.debug('failure: decodable_as_unicode: '
                  '%(decodable_as_unicode)r', locals())
+
+    return decodable_as_unicode
+
+
+def _check_has_null_bytes(bytes_to_check):
+    """
+    :param bytes_to_check: A chunk of bytes to check
+    :return: True if it contains null bytes otherwise False.
+    """
+    if b'\x00' in bytes_to_check or b'\xff' in bytes_to_check:
+        # Check for NULL bytes last
+        logger.debug('has nulls:' + repr(b'\x00' in bytes_to_check))
+        return True
+    else:
+        return False
+
+
+def is_binary_string(bytes_to_check):
+    """
+    Uses a simplified version of the Perl detection algorithm,
+    based roughly on Eli Bendersky's translation to Python:
+    http://eli.thegreenplace.net/2011/10/19/perls-guess-if-file-is-text-or-binary-implemented-in-python/
+
+    This is biased slightly more in favour of deeming files as text
+    files than the Perl algorithm, since all ASCII compatible character
+    sets are accepted as text, not just utf-8.
+
+    :param bytes_to_check: A chunk of bytes to check.
+    :returns: True if appears to be a binary, otherwise False.
+    """
+
+    # Empty files are considered text files
+    if not bytes_to_check:
+        return False
+
+    is_likely_binary = _check_is_likely_binary(bytes_to_check)
+    decodable_as_unicode = _check_is_decodable_as_unicode(bytes_to_check)
+
     if is_likely_binary:
         if decodable_as_unicode:
             return False
@@ -125,8 +160,4 @@ def is_binary_string(bytes_to_check):
         if decodable_as_unicode:
             return False
         else:
-            if b'\x00' in bytes_to_check or b'\xff' in bytes_to_check:
-                # Check for NULL bytes last
-                logger.debug('has nulls:' + repr(b'\x00' in bytes_to_check))
-                return True
-        return False
+            return _check_has_null_bytes(bytes_to_check)
